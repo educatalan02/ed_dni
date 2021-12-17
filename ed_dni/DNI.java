eaa9ee30d7078f3cbd98878a1e40d38968ac4048
Edu <educatalan02@gmail.com>
2021-12-17 20:01:09 +0100

    mostrarLetraDNI 1 step

--- ed_dni/DNI.java
+++ ed_dni/DNI.java
@@ -2,22 +2,42 @@ package ed_dni;
 
 public class DNI {
 
-    final char[] letra = { 'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V',
+    final static char[] letra = { 'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q',
+            'V',
             'H', 'L', 'C', 'K', 'E' };
 
     public static void main(String[] args) {
 
-        if (args.length == 0) {
-            System.out.println("ERROR: Debes pasar el numero de dni como argumento.");
-        } else {
-            String argDNI = args.toString().trim();
-
-        }
+        mostrarLetraDNI("18461590");
+        /*
+         * if (args.length == 0) {
+         * System.out.println("ERROR: Debes pasar el numero de dni como argumento.");
+         * } else {
+         * String argDNI = args.toString().trim();
+         * 
+         * mostrarLetraDNI(argDNI);
+         * 
+         * }
+         */
 
     }
-    public void mostrarLetraDNI(int letra){
-        letra = numeroDNI % 23;
-        System.out.println("La letra del DNI " + numeroDNI + " es " + letra);
-}
+
+    public static void mostrarLetraDNI(String dni) {
+
+        char letter = (char) (Integer.parseInt(dni) % 23);
+
+        String aux = "";
+
+        switch (letter) {
+            case 0:
+                aux += letra[0];
+                break;
+            case 1:
+                aux += letra[1];
+                break;
+        }
+
+        System.out.println("La letra del DNI " + dni + " es " + aux);
+    }
 
 }

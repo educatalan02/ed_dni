9534a536b73a65af0af0f6df958f9d5b70daa005
Jordi <jborrasr@iesch.org>
2021-12-17 19:37:19 +0100

    asd

--- DNI.java
+++ DNI.java
@@ -5,4 +5,10 @@ public class DNI{
         
     }
 
+    public void mostrarLetraDNI(int letra){
+            letra = numeroDNI % 23;
+            System.out.println("La letra del DNI " + numeroDNI + " es " + letra);
+    }
+
+
 }

b0d6e9c6f66f776095e98898eed4e622f86b7c77
Edu <educatalan02@gmail.com>
2021-12-17 19:34:10 +0100

    Second commit

--- ed_dni/DNI.java
+++ ed_dni/DNI.java
@@ -2,7 +2,8 @@ package ed_dni;
 
 public class DNI {
 
-    final String letra = "TRWAGMYFPDXBNJZSQVHLCKE";
+    final char[] letra = { 'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V',
+            'H', 'L', 'C', 'K', 'E' };
 
     public static void main(String[] args) {
 

773c05c309cf4716dc22d3f291db75b03e319715
Edu <educatalan02@gmail.com>
2021-12-17 19:27:36 +0100

    First upload

---
+++ ed_dni/DNI.java
@@ -0,0 +1,18 @@
+package ed_dni;
+
+public class DNI {
+
+    final String letra = "TRWAGMYFPDXBNJZSQVHLCKE";
+
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("ERROR: Debes pasar el numero de dni como argumento.");
+        } else {
+            String argDNI = args.toString().trim();
+
+        }
+
+    }
+
+}

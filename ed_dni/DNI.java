02a27b1ae52dcf0b5d6717f6cd50b063e261cd11
Edu <educatalan02@gmail.com>
2021-12-20 19:23:15 +0100

    A

--- ed_dni/DNI.java
+++ ed_dni/DNI.java
@@ -4,11 +4,12 @@ public class DNI {
 
     public static void main(String[] args) {
 
-       
     }
-    public static char mostrarLetraDNI(int n){
-        char letra[] ={'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'};
-        return letra[n%23];
+
+    public static char mostrarLetraDNI(int n) {
+        char letra[] = { 'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H',
+                'L', 'C', 'K', 'E' };
+        return letra[n % 23];
     }
 
 }

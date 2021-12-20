bf751c713851a9d6b42128d619db39228d86800d
Edu <educatalan02@gmail.com>
2021-12-20 19:11:34 +0100

    Merge edu

--- ed_dni/DNI.java
+++ ed_dni/DNI.java
@@ -4,7 +4,6 @@ public class DNI {
 
     public static void main(String[] args) {
 
-<<<<<<< HEAD
         // System.out.println(crearCadenaDNI(18461590));
     }
 
@@ -12,13 +11,6 @@ public class DNI {
         
 
         return String.format("%8s", Integer.toString(n)).replace(" ", "0") + mostrarLetraDNI(n));
-=======
-       
-    }
-    public static char mostrarLetraDNI(int n){
-        char letra[] ={'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'};
-        return letra[n%23];
->>>>>>> jordi
     }
 
 }

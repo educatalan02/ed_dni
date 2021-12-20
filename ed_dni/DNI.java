fd6e503912b4f5eefeb32ca937bb91792b7fe040
Roberto <rbrunb@iesch.org>
2021-12-20 18:40:52 +0400

    Falta poner que parametro imprime

--- ed_dni/DNI.java
+++ ed_dni/DNI.java
@@ -4,6 +4,15 @@ public class DNI {
 
     public static void main(String[] args) {
 
+        if (args.length==0){
+            System.out.println("ERROR: Debes pasar el numero del DNI como argumento");
+        } else{
+            System.out.println(mostrarLetraDNI());
+            System.out.println(crearCadenaDNI());
+        }
+
+       
+        
     }
 
 }

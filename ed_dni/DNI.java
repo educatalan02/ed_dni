4dc85ed33c93ff7683cb97ab1730e4faaa9b7196
Jordi <jborrasr@iesch.org>
2021-12-20 18:37:08 +0100

    a

--- ed_dni/DNI.java
+++ ed_dni/DNI.java
@@ -4,6 +4,11 @@ public class DNI {
 
     public static void main(String[] args) {
 
+       
+    }
+    public static char mostrarLetraDNI(int n){
+        char letra[] ={'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'};
+        return letra[n%23];
     }
 
 }

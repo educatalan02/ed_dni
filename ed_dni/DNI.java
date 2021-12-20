e7a660a66a3c351681410ec2b6af06b271f14167
Edu <educatalan02@gmail.com>
2021-12-20 18:51:04 +0100

    Terminado

--- ed_dni/DNI.java
+++ ed_dni/DNI.java
@@ -4,6 +4,13 @@ public class DNI {
 
     public static void main(String[] args) {
 
+        // System.out.println(crearCadenaDNI(18461590));
+    }
+
+    public static String crearCadenaDNI(int n) {
+        String dni = Integer.toString(n);
+
+        return String.format("%8s", dni).replace(" ", "0") + mostrarLetraDNI(n));
     }
 
 }
